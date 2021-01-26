c2346bbbeae2189269d4a297e3bdc6402eaf4b3c
nm-mutia <nm.mutia02@gmail.com>
2021-01-27 02:23:06 +0700

    update to upload

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright ⓒ {year} <a href="https://github.com/nm-mutia/keeper-app" target="_blank">nmmutia</a></p>
     </footer>
   );
 }
